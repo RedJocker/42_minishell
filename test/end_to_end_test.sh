280516a71ac18150949188e14c831b901ced2be8
redjocker <mauricio.guaruja@gmail.com>
2024-08-15 18:59:13 -0300

    Feat: add first end to end tests

---
+++ test/end_to_end_test.sh
@@ -0,0 +1,69 @@
+#!/bin/env bash -e
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    end_to_end_test.sh                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/15 18:09:18 by maurodri          #+#    #+#              #
+#    Updated: 2024/08/15 18:58:35 by maurodri         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+setup_file() {
+    #echo "START TEST" 1>&3
+    make -C ..
+}
+
+setup() {
+    #echo "START TEST METHOD" 1>&3
+    true
+}
+
+teardown() {
+    #echo "END TEST METHOD" 1>&3
+    true
+}
+
+teardown_file() {
+    #echo "END TEST" 1>&3
+    true
+}
+
+bash_execute() {
+    bash <<< "$@"
+}
+
+minishell_execute() {
+    ../minishell <<< "$@"
+}
+
+assert_minishell_equal_bash() {
+    run bash_execute $@
+    local bash_status=$status
+    local bash_output=$output
+
+    #echo $bash_status 1>&3
+    #echo $bash_output 1>&3
+
+    run minishell_execute $@
+    if ! [[ $bash_output == $output ]]; then
+	echo -e "===> bash_output:\n<$bash_output>\n===> minishell_output:\n<$output>"
+	false
+    fi
+
+    if ! [[ $bash_status == $status ]]; then
+	echo -e "===> bash_status: $bash_status\nminishell_status: $status"
+	false
+    fi
+}
+
+@test "test simple command: ls" {
+    assert_minishell_equal_bash ls
+}
+
+@test "test simple command with one arg: ls -l" {
+    assert_minishell_equal_bash ls -l
+}
