edf9609c55fe1b3bdc1fcd5d06db62ede115a841
dande-je <dande-je@student.42sp.org.br>
2024-08-15 05:08:43 -0300

    Feat: Add init project, the libftx it is add and Makefile is building.  Changes to be committed: 	modified:   .gitignore 	new file:   Makefile 	new file:   src/main.c

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 01:25:54 by dande-je          #+#    #+#             */
+/*   Updated: 2024/08/15 05:07:50 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stdio.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_printf("minishell.\n");
+	exit(EXIT_SUCCESS);
+}
