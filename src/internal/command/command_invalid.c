bade28149036da90730d6736cf5a335d85a5d9ea
dande-je <dande-je@student.42sp.org.br>
2024-08-26 03:41:10 -0300

    Fix: Uninitialized variable.  Changes to be committed: 	modified:   src/internal/command/command_invalid.c

--- src/internal/command/command_invalid.c
+++ src/internal/command/command_invalid.c
@@ -6,20 +6,20 @@
 /*   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 20:08:23 by maurodri          #+#    #+#             */
-/*   Updated: 2024/08/25 22:01:35 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/08/26 03:21:32 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "command_internal.h"
 #include "ft_memlib.h"
 #include "ft_string.h"
-#include "internal/command/command.h"
 #include "minishell.h"
 
 t_command	command_invalid_new(char *message)
 {
 	t_command	cmd;
 
+	cmd = NULL;
 	command_new(CMD_INVALID, "CMD_INVALID");
 	cmd->invalid = ft_calloc(1, sizeof(t_command_invalid));
 	if (!cmd->invalid)
