230985df338ab6ac23f8f0280f2a15a3370b69de
dande-je <dande-je@student.42sp.org.br>
2024-09-09 04:28:48 -0300

    Refactor: Applying norminette, add TODOS and refactor the logic of echo builtin.  Changes to be committed: 	modified:   src/internal/runner/builtins/echo.c

--- src/internal/runner/builtins/echo.c
+++ src/internal/runner/builtins/echo.c
@@ -6,54 +6,73 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 04:55:24 by dande-je          #+#    #+#             */
-/*   Updated: 2024/09/09 03:22:58 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/09/09 04:12:31 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
 #include <unistd.h>
 #include "ft_assert.h"
 #include "ft_string.h"
 #include "internal/command/command.h"
 #include "internal/command/io_handler.h"
+#include "internal/default.h"
+
+static int	set_out_len(t_command cmd, char **arg, int *out_fd);
+static void	echo_display(t_command cmd, char **arg, int *out_fd);
 
 int	runner_cmd_builtin_echo(t_command cmd)
 {
-	char			*arg;
-	int				len_arg;
-	int				i;
-	int             out_fd;
-	int				out_len;
-	const char		*space_newline = " \n";
+	char	*arg;
+	int		out_fd;
 
 	out_fd = STDOUT_FILENO;
+	if (!set_out_len(cmd, &arg, &out_fd))
+		return (STDERR_FILENO);
+	echo_display(cmd, &arg, &out_fd);
+	if (out_fd != STDOUT_FILENO)
+		close(out_fd);
+	return (0); // TODO:Verifiy which name can be used to remove magic number.
+}
+
+static int	set_out_len(t_command cmd, char **arg, int *out_fd)
+{
+	int	out_len;
+
 	out_len = ft_arraylist_len(cmd->output);
 	if (out_len)
 	{
-		if (!io_handlers_to_fd(cmd->output, &arg))
+		if (!io_handlers_to_fd(cmd->output, arg))
 		{
-			ft_assert(0, arg); // TODO handle error on redirect like cmd_simple
-			return (2);
+			ft_assert(0, *arg); // TODO: handle error on redirect like cmd_simple
+			return (FAIL);
 		}
-		out_fd = ((t_io_handler *)
-			ft_arraylist_get(cmd->output, out_len - 1))->fd;
+		*out_fd = ((t_io_handler *)
+			ft_arraylist_get(cmd->output, out_len - 1))->fd; // TODO:Verifiy which name can be used to remove magic number.
 	}
-	i = 0;
-	while (++i < cmd->simple->cmd_argc - 1)
+	return (true);
+}
+
+static void	echo_display(t_command cmd, char **arg, int *out_fd)
+{
+	int				len_arg;
+	int				i;
+	const char		*space_newline = " \n";
+
+	i = DEFAULT;
+	len_arg = DEFAULT;
+	while (++i < cmd->simple->cmd_argc - 1) // TODO:Verifiy which name can be used to remove magic number.
 	{
-		arg = cmd->simple->cmd_argv[i];
-		len_arg = ft_strlen(arg);
-		write(out_fd, arg, len_arg);
-		write(out_fd, &(space_newline[0]), 1);
+		*arg = cmd->simple->cmd_argv[i];
+		len_arg = ft_strlen(*arg);
+		write(*out_fd, *arg, len_arg);
+		write(*out_fd, &(space_newline[0]), STDOUT_FILENO); // TODO:Verifiy which name can be used to remove magic number.
 	}
 	if (i < cmd->simple->cmd_argc)
 	{
-		arg = cmd->simple->cmd_argv[i];
-		len_arg = ft_strlen(arg);
-		write(out_fd, arg, len_arg);
+		*arg = cmd->simple->cmd_argv[i];
+		len_arg = ft_strlen(*arg);
+		write(*out_fd, *arg, len_arg);
 	}
-	write(out_fd, &(space_newline[1]), 1);
-	if (out_fd != STDOUT_FILENO)
-		close(out_fd);
-	return (0);
+	write(*out_fd, &(space_newline[1]), STDOUT_FILENO); // TODO:Verifiy which name can be used to remove magic number.
 }
-
