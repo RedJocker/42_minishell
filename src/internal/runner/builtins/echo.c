7fde8cf1948cdd553a5e8e3b2d623fb81dad22d3
dande-je <dande-je@student.42sp.org.br>
2024-09-09 05:30:51 -0300

    WIP: Trying to fix echo with invalid redirect.  Changes to be committed: 	modified:   src/internal/runner/builtins/echo.c

--- src/internal/runner/builtins/echo.c
+++ src/internal/runner/builtins/echo.c
@@ -6,13 +6,12 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 04:55:24 by dande-je          #+#    #+#             */
-/*   Updated: 2024/09/09 04:12:31 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/09/09 05:24:58 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
 #include <unistd.h>
-#include "ft_assert.h"
 #include "ft_string.h"
 #include "internal/command/command.h"
 #include "internal/command/io_handler.h"
@@ -28,7 +27,10 @@ int	runner_cmd_builtin_echo(t_command cmd)
 
 	out_fd = STDOUT_FILENO;
 	if (!set_out_len(cmd, &arg, &out_fd))
-		return (STDERR_FILENO);
+	{
+		command_destroy(cmd);
+		return (2); // FIX: Is this status code?
+	}
 	echo_display(cmd, &arg, &out_fd);
 	if (out_fd != STDOUT_FILENO)
 		close(out_fd);
@@ -43,10 +45,7 @@ static int	set_out_len(t_command cmd, char **arg, int *out_fd)
 	if (out_len)
 	{
 		if (!io_handlers_to_fd(cmd->output, arg))
-		{
-			ft_assert(0, *arg); // TODO: handle error on redirect like cmd_simple
 			return (FAIL);
-		}
 		*out_fd = ((t_io_handler *)
 			ft_arraylist_get(cmd->output, out_len - 1))->fd; // TODO:Verifiy which name can be used to remove magic number.
 	}
