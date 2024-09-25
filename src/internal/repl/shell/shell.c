1f9f137cf23010439b513145cad2485aa32ddf77
RedJocker <mauricio.guaruja@gmail.com>
2024-09-24 23:33:30 -0300

    Fix Test: protect file expansion to avoid non printables

--- src/internal/repl/shell/shell.c
+++ src/internal/repl/shell/shell.c
@@ -6,11 +6,12 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 06:34:42 by dande-je          #+#    #+#             */
-/*   Updated: 2024/09/24 20:17:42 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/09/24 22:37:44 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <readline/readline.h>
+#include "ft_util.h"
 #include "internal/default.h"
 #include "internal/repl/shell/parse/parse.h"
 #include "internal/repl/shell/runner/runner.h"
@@ -28,8 +29,10 @@ void	shell_set_input(t_shell *shell)
 void	shell_command(t_shell *shell)
 {
 	shell->str_tokens = parse_input(shell->input);
+	//ft_strarr_printfd(shell->str_tokens, 1);
 	shell->tokens = tokens_classify(shell->str_tokens, \
 						&shell->tokens_len);
+	//tokens_print(shell->tokens);
 	shell->command = command_build(shell->tokens, shell->tokens_len);
 	tokens_destroy(shell->tokens);
 	shell->status = runner(shell->command, shell->status);
