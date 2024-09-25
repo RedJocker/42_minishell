0e16457a6eef55a1c1ed7e8488aa39d949b2ce20
RedJocker <mauricio.guaruja@gmail.com>
2024-09-25 00:24:19 -0300

    Refactor: norminette expand

--- src/internal/repl/shell/runner/expand/expand.c
+++ src/internal/repl/shell/runner/expand/expand.c
@@ -6,12 +6,13 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 20:38:29 by maurodri          #+#    #+#             */
-/*   Updated: 2024/09/24 20:57:47 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/09/25 00:20:01 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expand.h"
 #include "ft_string.h"
+#include "internal/repl/shell/runner/expand/expand_internal.h"
 #include "stringbuilder.h"
 #include "ft_ctype.h"
 #include "ft_assert.h"
@@ -25,8 +26,8 @@ static char	*env_mock(char *s)
 	return (ft_strdup("abc def"));
 }
 
-static int	expand_dollar(char *str, t_stringbuilder *builder, \
-		sig_atomic_t last_status_code)
+static int	expand_dollar(
+	char *str, t_stringbuilder *builder, sig_atomic_t last_status_code)
 {
 	int		i;
 	char	*var_name;
@@ -40,96 +41,95 @@ static int	expand_dollar(char *str, t_stringbuilder *builder, \
 		free(var_value);
 		return (1);
 	}
-	i = 1;
-	if (ft_isalpha(str[i]) || str[i] == '_')
-	{
-		i++;
+	i = 0;
+	if (ft_isalpha(str[++i]) || str[i] == '_')
 		while (ft_isalnum(str[i]) || str[i] == '_')
 			i++;
-	}
 	var_name = ft_calloc(i, sizeof(char));
 	ft_strlcpy(var_name, str + 1, i);
-	// TODO: look expansion on env
-	var_value = env_mock(var_name);
+	var_value = env_mock(var_name); // TODO: look expansion on env
 	free(var_name);
 	stringbuilder_addstr(builder, var_value);
 	free(var_value);
 	return (i - 1);
 }
 
-static int	expand_dollar_split(char *str, t_stringbuilder *builder, \
-		t_arraylist *lst_new_args, sig_atomic_t last_status_code)
+static int	expand_dollar_split(
+	char *str, t_expansion_state *state, sig_atomic_t last_status_code)
 {
 	int		i;
 	char	**split;
 	char	*temp;
 	int		j;
 
-	i = expand_dollar(str, builder, last_status_code);
-	temp = stringbuilder_build(*builder);
+	i = expand_dollar(str, &state->builder, last_status_code);
+	temp = stringbuilder_build(state->builder);
 	split = ft_splitfun(temp, (t_pred_int) ft_isspace);
 	free(temp);
 	j = -1;
 	while (split[++j + 1])
-		*lst_new_args = ft_arraylist_add(*lst_new_args, ft_strdup(split[j]));
-	*builder = stringbuilder_new();
-	stringbuilder_addstr(builder, split[j]);
+		state->lst_new_args = ft_arraylist_add(
+				state->lst_new_args, ft_strdup(split[j]));
+	state->builder = stringbuilder_new();
+	stringbuilder_addstr(&state->builder, split[j]);
 	ft_strarr_free(split);
 	return (i);
 }
 
-static void	expand_str(char *str, sig_atomic_t last_status_code, \
-			t_arraylist *lst_new_args)
+static void	expand_str_noquote(
+	char *str, sig_atomic_t last_status_code, t_expansion_state *state)
 {
-	t_stringbuilder	builder;
-	char			*res;
-	char			open_quote;
-	int				i;
+	if (str[state->cur_ch] == '$')
+		state->cur_ch += expand_dollar_split(
+				str + state->cur_ch, state, last_status_code);
+	else if (str[state->cur_ch] == '\'' || str[state->cur_ch] == '\"')
+		state->open_quote = str[state->cur_ch];
+	else
+		state->builder = stringbuilder_addchar(
+				state->builder, str[state->cur_ch]);
+}
 
-	builder = stringbuilder_new();
-	open_quote = 0;
-	i = -1;
-	while (str[++i])
+static void	expand_str(
+	char *str, sig_atomic_t last_status_code, t_expansion_state *state)
+{
+	state->builder = stringbuilder_new();
+	state->open_quote = 0;
+	state->cur_ch = -1;
+	while (str[++(state->cur_ch)])
 	{
-		if (open_quote == 0)
-		{
-			if (str[i] == '$')
-				i += expand_dollar_split(str + i, &builder, lst_new_args, last_status_code);
-			else if (str[i] == '\'' || str[i] == '\"')
-				open_quote = str[i];
-			else
-				builder = stringbuilder_addchar(builder, str[i]);
-		}
+		if (state->open_quote == 0)
+			expand_str_noquote(str, last_status_code, state);
 		else
 		{
-			if (str[i] == open_quote)
-				open_quote = 0;
-			else if (open_quote == '\"' && str[i] == '$')
-				i += expand_dollar(str + i, &builder, last_status_code);
+			if (str[state->cur_ch] == state->open_quote)
+				state->open_quote = 0;
+			else if (state->open_quote == '\"' && str[state->cur_ch] == '$')
+				state->cur_ch += expand_dollar(
+						str + state->cur_ch, &state->builder, last_status_code);
 			else
-				builder = stringbuilder_addchar(builder, str[i]);
+				state->builder = stringbuilder_addchar(
+						state->builder, str[state->cur_ch]);
 		}
 	}
-	res = stringbuilder_build(builder);
-	*lst_new_args = (ft_arraylist_add(*lst_new_args, res));
+	state->res = stringbuilder_build(state->builder);
+	state->lst_new_args = (ft_arraylist_add(state->lst_new_args, state->res));
 }
 
 void	expand(t_command cmd, sig_atomic_t last_status_code)
 {
-	t_arraylist	lst_new_args;
-	int			i;
+	t_expansion_state	state;
+	int					i;
 
 	if (cmd->type != CMD_SIMPLE)
 		return ;
-	lst_new_args = ft_arraylist_new(free);
+	state.lst_new_args = ft_arraylist_new(free);
 	i = -1;
 	while (cmd->simple->cmd_argv[++i])
 		expand_str(
-			cmd->simple->cmd_argv[i], last_status_code, &lst_new_args);
-	// TODO: improve allocation error handling
+			cmd->simple->cmd_argv[i], last_status_code, &state);
 	ft_strarr_free(cmd->simple->cmd_argv);
-	cmd->simple->cmd_argv = ft_lststr_to_arrstr(lst_new_args);
-	ft_arraylist_destroy(lst_new_args);
+	cmd->simple->cmd_argv = ft_lststr_to_arrstr(state.lst_new_args);
+	ft_arraylist_destroy(state.lst_new_args);
 	cmd->simple->cmd_argc = 0;
 	while (cmd->simple->cmd_argv[cmd->simple->cmd_argc])
 		cmd->simple->cmd_argc++;
--- src/internal/repl/shell/runner/expand/expand_internal.h
+++ src/internal/repl/shell/runner/expand/expand_internal.h
@@ -1,22 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expand.h                                           :+:      :+:    :+:   */
+/*   expand_internal.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/24 20:40:01 by maurodri          #+#    #+#             */
-/*   Updated: 2024/09/25 00:23:33 by maurodri         ###   ########.fr       */
+/*   Created: 2024/09/24 21:10:58 by maurodri          #+#    #+#             */
+/*   Updated: 2024/09/25 00:03:36 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXPAND_H
-# define EXPAND_H
+#ifndef EXPAND_INTERNAL_H
+# define EXPAND_INTERNAL_H
 
-# include "internal/repl/shell/command/command.h"
-# include "internal/repl/shell/runner/expand/expand_internal.h"
-# include <signal.h>
+# include "stringbuilder.h"
+# include "collection/ft_arraylist.h"
 
-void	expand(t_command cmd, sig_atomic_t last_status_code);
+typedef struct s_expansion_state
+{
+	t_stringbuilder	builder;
+	char			*res;
+	char			open_quote;
+	int				cur_ch;
+	t_arraylist		lst_new_args;
+}	t_expansion_state;
 
 #endif
