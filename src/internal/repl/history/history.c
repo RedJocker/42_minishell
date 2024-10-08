31f6fd22a1be6d1a916604218b2a34bff9b2cee4
willtrigo <willtrigo@gmail.com>
2024-10-08 05:50:45 -0300

    WIP: Maybe this implementation will be removed, it is about the history of the heredoc.  Changes to be committed: 	modified:   src/internal/repl/history/history.c

--- src/internal/repl/history/history.c
+++ src/internal/repl/history/history.c
@@ -6,17 +6,21 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 05:57:22 by dande-je          #+#    #+#             */
-/*   Updated: 2024/09/19 06:10:25 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/10/08 05:46:29 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <readline/history.h>
 #include <stdbool.h>
 #include "ft_ctype.h"
+#include "ft_string.h"
+#include "internal/default.h"
 
 bool	history_add_input(char *input)
 {
 	if (!input)
+		return (true);
+	else if (ft_strncmp(input, "<<", CHAR_BYTE + CHAR_BYTE) == DEFAULT) //TODO: Verify this, "However, it doesn’t have to update the history!", this is in the pdf.
 		return (true);
 	while (*input)
 	{
