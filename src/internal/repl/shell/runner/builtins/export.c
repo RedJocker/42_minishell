a35c020fd1ca38c9498dc370a577c43170ca61c7
dande-je <dande-je@student.42sp.org.br>
2024-10-11 04:55:00 -0300

    WIP: First part of the export done, without args, get values from env and sort and print the value.  Changes to be committed: 	modified:   src/internal/repl/shell/runner/builtins/export.c

--- src/internal/repl/shell/runner/builtins/export.c
+++ src/internal/repl/shell/runner/builtins/export.c
@@ -6,79 +6,71 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 00:46:01 by maurodri          #+#    #+#             */
-/*   Updated: 2024/09/25 00:48:10 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/10/11 03:42:26 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdbool.h>
 #include <unistd.h>
 #include "ft_string.h"
 #include "ft_stdio.h"
+#include "ft_util.h"
 #include "internal/default.h"
+#include "internal/env/envp.h"
 #include "internal/repl/shell/command/command.h"
-#include "internal/repl/shell/command/io_handler.h"
 
-static int	set_out_len(t_command cmd, char **arg, int *out_fd);
-static void	export_display(t_command cmd, char **arg, int *out_fd);
+static void	export_without_args(void);
+static void	export_sort_vars(char **envp);
+static void	export_print_vars(char **envp);
 
 int	runner_cmd_builtin_export(t_command cmd)
 {
-	char	*arg;
-	int		out_fd;
-
-	out_fd = STDOUT_FILENO;
-	if (!set_out_len(cmd, &arg, &out_fd))
-	{
-		command_destroy(cmd);
-		return (2); // FIX: Is this status code?
-	}
-	export_display(cmd, &arg, &out_fd);
-	if (out_fd != STDOUT_FILENO)
-		close(out_fd);
-	return (0); // TODO:Verifiy which name can be used to remove magic number.
+	if (DEFAULT_BEGIN == cmd->simple->cmd_argc)
+		export_without_args();
+	return (EXIT_OK);
 }
 
-static int	set_out_len(t_command cmd, char **arg, int *out_fd)
+static void	export_without_args(void)
 {
-	int	out_len;
+	char	**envp;
 
-	out_len = ft_arraylist_len(cmd->io_handlers);
-	if (out_len)
-	{
-		if (!io_handlers_to_fd(cmd->io_handlers, arg))
-			return (FAIL);
-		*out_fd = ((t_io_handler *)
-				ft_arraylist_get(cmd->io_handlers, out_len - 1))->fd;
-	}
-	return (true);
+	envp = get_envp();
+	export_sort_vars(envp);
+	export_print_vars(envp);
+	if (envp)
+		ft_strarr_free(envp);
 }
 
-static void	export_display(t_command cmd, char **arg, int *out_fd)
+static void	export_sort_vars(char **envp)
 {
-	char	*argc_value;
+	char	*swap;
+	int		i;
+	int		next_i;
 
-	ft_asprintf(&argc_value, "%d", cmd->simple->cmd_argc);
-	write(*out_fd, "argc - ", ft_strlen("argc - "));
-	write(*out_fd, argc_value, ft_strlen(argc_value));
-	write(*out_fd, "\n", ft_strlen("\n"));
-	int				len_arg;
-	int				i;
-	const char		*space_newline = " \n";
+	i = DEFAULT_INIT;
+	while (envp[++i])
+	{
+		next_i = i;
+		while (envp[++next_i])
+		{
+			if (ft_strncmp(envp[i], envp[next_i], \
+				ft_strlen(envp[next_i])) > DEFAULT)
+			{
+				swap = envp[i];
+				envp[i] = envp[next_i];
+				envp[next_i] = swap;
+			}
+		}
+	}
+}
 
-	i = DEFAULT;
-	len_arg = DEFAULT;
-	while (++i < cmd->simple->cmd_argc - 1) // TODO:Verifiy which name can be used to remove magic number.
+static void	export_print_vars(char **envp)
+{
+	char	*env_var;
+
+	while (envp && *envp)
 	{
-		*arg = cmd->simple->cmd_argv[i];
-		len_arg = ft_strlen(*arg);
-		write(*out_fd, *arg, len_arg);
-		write(*out_fd, &(space_newline[0]), STDOUT_FILENO); // TODO:Verifiy which name can be used to remove magic number.
+		ft_asprintf(&env_var, "declare -x %s\n", *envp++);
+		write(STDOUT_FILENO, env_var, ft_strlen(env_var));
+		free(env_var);
 	}
-	if (i < cmd->simple->cmd_argc)
-	{
-		*arg = cmd->simple->cmd_argv[i];
-		len_arg = ft_strlen(*arg);
-		write(*out_fd, *arg, len_arg);
-	}
-	write(*out_fd, &(space_newline[1]), STDOUT_FILENO); // TODO:Verifiy which name can be used to remove magic number.
 }
