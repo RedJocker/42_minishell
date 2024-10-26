abdd04f0e144062fff4ca204b67d0fd2667f3bca
dande-je <dande-je@student.42sp.org.br>
2024-10-26 05:06:09 -0300

    Fix: Fix error output of cd and fix update of pwd when change folders.  Changes to be committed: 	modified:   src/internal/repl/shell/runner/builtins/cd.c

--- src/internal/repl/shell/runner/builtins/cd.c
+++ src/internal/repl/shell/runner/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 02:17:40 by dande-je          #+#    #+#             */
-/*   Updated: 2024/10/24 06:02:07 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/10/26 05:04:01 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include "internal/repl/shell/command/command.h"
 
 static char			*get_pwd(void);
-static sig_atomic_t	print_cd_error(char *msg, sig_atomic_t status);
+static sig_atomic_t	print_cd_error(char *msg, char *path, sig_atomic_t status);
 static sig_atomic_t	cd_change_folder(char *path, sig_atomic_t status);
 static void			update_value(char *key);
 
@@ -29,14 +29,14 @@ sig_atomic_t	runner_cmd_builtin_cd(t_command cmd)
 
 	path = NULL;
 	if (cmd->simple->cmd_argc > MAX_ARGS)
-		return (print_cd_error("too many arguments", EXIT_FAILURE));
+		return (print_cd_error("too many arguments", NULL, EXIT_FAILURE));
 	else if (!cmd->simple->cmd_argv[DEFAULT_BEGIN] \
 		|| ft_strncmp(cmd->simple->cmd_argv[DEFAULT_BEGIN], "~", \
 		CHAR_BYTE + NULL_BYTE) == DEFAULT)
 	{
 		path = env_get_value("HOME");
 		if (!path)
-			return (print_cd_error("HOME not set", EXIT_FAILURE));
+			return (print_cd_error("HOME not set", NULL, EXIT_FAILURE));
 	}
 	else
 		path = ft_strdup(cmd->simple->cmd_argv[DEFAULT_BEGIN]);
@@ -45,16 +45,16 @@ sig_atomic_t	runner_cmd_builtin_cd(t_command cmd)
 
 static sig_atomic_t	cd_change_folder(char *path, sig_atomic_t status)
 {
-	if (access(path, F_OK | R_OK) == DEFAULT)
-		update_value("OLDPWD");
 	if (access(path, F_OK))
-		status = print_cd_error("no such file or directory", EXIT_FAILURE);
+		status = print_cd_error("No such file or directory", path, EXIT_FAILURE);
 	else if (access(path, R_OK))
-		status = print_cd_error("permission denied", EXIT_FAILURE);
+		status = print_cd_error("Permission denied", path, EXIT_FAILURE);
 	else
+	{
+		update_value("OLDPWD");
 		chdir(path);
-	if (access(path, F_OK | R_OK) == DEFAULT)
 		update_value("PWD");
+	}
 	if (path)
 		free(path);
 	return (status);
@@ -85,12 +85,16 @@ static char	*get_pwd(void)
 	return (pwd);
 }
 
-static sig_atomic_t	print_cd_error(char *msg, sig_atomic_t status)
+static sig_atomic_t	print_cd_error(char *msg, char *path, sig_atomic_t status)
 {
 	char	*error_msg;
 
-	ft_asprintf(&error_msg, \
-		"bash: cd: %s\n", msg); // TODO: change the name of program to minishell instead of bash.
+	if (!path)
+		ft_asprintf(&error_msg, \
+			"bash: cd: %s\n", msg); // TODO: change the name of program to minishell instead of bash.
+	else
+		ft_asprintf(&error_msg, \
+			"bash: cd: %s: %s\n", path, msg); // TODO: change the name of program to minishell instead of bash.
 	write(STDOUT_FILENO, error_msg, ft_strlen(error_msg));
 	free(error_msg);
 	return (status);
