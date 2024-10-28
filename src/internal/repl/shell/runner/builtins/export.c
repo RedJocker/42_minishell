1d14697081d37fed58f1bf05891b0b839527ce3b
dande-je <dande-je@student.42sp.org.br>
2024-10-28 02:40:33 -0300

    Fix: Add "\" when a var from env have "$" inside of his value.  Changes to be committed: 	modified:   src/internal/repl/shell/runner/builtins/export.c

--- src/internal/repl/shell/runner/builtins/export.c
+++ src/internal/repl/shell/runner/builtins/export.c
@@ -6,18 +6,18 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 00:46:01 by maurodri          #+#    #+#             */
-/*   Updated: 2024/10/27 05:29:32 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/10/28 02:31:18 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include "collection/ft_arraylist.h"
 #include "ft_ctype.h"
 #include "ft_string.h"
 #include "ft_stdio.h"
 #include "ft_util.h"
+#include "stringbuilder.h"
 #include "internal/default.h"
 #include "internal/env/env.h"
 #include "internal/env/env_internal/env_internal.h"
@@ -27,22 +27,24 @@
 static sig_atomic_t	export_args(char *cmd, sig_atomic_t status);
 static void			export_valid_arg(char *cmd);
 static void			export_sort_vars(char **envp);
-static void			export_print_vars(char **envp);
+static void			export_print_vars(char **envp, int envp_i, t_stringbuilder *builder_new_envp);
 
 sig_atomic_t	runner_cmd_builtin_export(t_command cmd)
 {
 	int				i;
 	sig_atomic_t	status;
 	char			**envp;
+	t_stringbuilder	builder_new_envp;
 
 	i = DEFAULT;
 	status = EXIT_OK;
 	envp = NULL;
+	builder_new_envp = stringbuilder_new();
 	if (DEFAULT_BEGIN == cmd->simple->cmd_argc)
 	{
 		envp = get_envp(ENVP_EXPORT);
 		export_sort_vars(envp);
-		export_print_vars(envp);
+		export_print_vars(envp, DEFAULT_INIT, &builder_new_envp);
 		if (envp)
 			ft_strarr_free(envp);
 	}
@@ -50,6 +52,7 @@ sig_atomic_t	runner_cmd_builtin_export(t_command cmd)
 		while (cmd->simple->cmd_argv[++i])
 			if (export_args(cmd->simple->cmd_argv[i], status) == EXIT_FAILURE)
 				status = EXIT_FAILURE;
+	stringbuilder_destroy(builder_new_envp);
 	return (status);
 }
 
@@ -67,10 +70,11 @@ static sig_atomic_t	export_args(char *cmd, sig_atomic_t status)
 	{
 		status = EXIT_FAILURE;
 		ft_asprintf(&error_msg, \
-			"minishell: export: '%s': not a valid identifier\n", cmd);
-		write(STDOUT_FILENO, error_msg, ft_strlen(error_msg));
+			"minishell: export: `%s': not a valid identifier\n", cmd);
+		write(STDERR_FILENO, error_msg, ft_strlen(error_msg));
 		free(error_msg);
 	}
+	free(key);
 	return (status);
 }
 
@@ -115,30 +119,28 @@ static void	export_sort_vars(char **envp)
 	}
 }
 
-static void	export_print_vars(char **envp)
+static void	export_print_vars(char **envp, int envp_i, t_stringbuilder *builder_new_envp)
 {
-	int			envp_i;
-	t_arraylist	lst_new_envp;
-	char		*env_var;
+	char			*temp_envp;
+	char			*env_var;
 
-	lst_new_envp = NULL;
-	envp_i = DEFAULT_INIT;
 	while (envp && *envp)
 	{
 		if (!ft_strnstr(*envp, "$", ft_strlen(*envp)))
-			ft_asprintf(&env_var, "declare -x %s\n", *envp++);
+			ft_asprintf(&env_var, "declare -x %s\n", *envp);
 		else
 		{
-			lst_new_envp = ft_arraylist_new((t_consumer) free);
-			while (envp[++envp_i])
+			while (envp[DEFAULT][++envp_i])
 			{
-				if (*envp[envp_i] == '$')
-					lst_new_envp = ft_arraylist_add(lst_new_envp, "\\");
-				lst_new_envp = ft_arraylist_add(lst_new_envp, envp[envp_i]);
+				if (envp[DEFAULT][envp_i] == '$')
+					stringbuilder_addchar(*builder_new_envp, '\\');
+				stringbuilder_addchar(*builder_new_envp, envp[DEFAULT][envp_i]);
 			}
-			ft_asprintf(&env_var, "declare -x %s\n", ft_lststr_to_arrstr(lst_new_envp));
-			free(lst_new_envp);
+			temp_envp = stringbuilder_build(*builder_new_envp);
+			ft_asprintf(&env_var, "declare -x %s\n", temp_envp);
+			free(temp_envp);
 		}
+		envp++;
 		write(STDOUT_FILENO, env_var, ft_strlen(env_var));
 		free(env_var);
 	}
