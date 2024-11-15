3eccf8a5e31070494e4a20da7be12fc5ed5932ee
RedJocker <mauricio.guaruja@gmail.com>
2024-11-15 00:14:02 -0300

    Fix: does not segfault on unclosed paren

--- src/internal/repl/shell/command/command_build_paren.c
+++ src/internal/repl/shell/command/command_build_paren.c
@@ -6,7 +6,7 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 22:05:18 by maurodri          #+#    #+#             */
-/*   Updated: 2024/11/14 00:58:46 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/11/15 00:13:01 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,46 +38,38 @@ static int	find_paren_end(t_token **tokens, int tokens_len)
 			open_paren--;
 		if (open_paren == 0)
 			break ;
+		else if (token_type_is_either_nl_eof(tokens[i]))
+			break ;
 		i++;
 	}
 	return (i);
-}
-
-int	is_valid_end(t_token *token)
-{
-	return (token
-		&& (token->type == OP_EOF
-			|| token->type == OP_NEWLINE
-			|| token->type == OP_PIPE
-			|| token->type == OP_AND
-			|| token->type == OP_OR
-			|| token->type == OP_PAREN_CLOSE));
 }
 
 t_command	command_build_parentheses(
 	t_token **tokens, int op_idx, int tokens_len)
 {
 	t_command	cmd;
-	int			paren_close_idx;
+	int			close_i;
 
 	(void) op_idx;
 	ft_assert(tokens[0]->type == OP_PAREN_OPEN,
 		"expected opening paren at command_cuild_parentheses");
-	paren_close_idx = find_paren_end(tokens, tokens_len);
-	if (tokens[paren_close_idx]->type != OP_PAREN_CLOSE \
-		|| !is_valid_end(tokens[paren_close_idx + 1]) \
-		|| paren_close_idx < 2)
+	close_i = find_paren_end(tokens, tokens_len);
+	if (tokens[close_i]->type != OP_PAREN_CLOSE \
+		|| !token_type_is_end_op(tokens[close_i + 1]) \
+		|| close_i < 2)
 	{
-		paren_close_idx += (!is_valid_end(tokens[paren_close_idx + 1]) \
-								&& paren_close_idx > 1);
-		return (command_build_panic_zero(tokens[paren_close_idx]->content));
+		close_i += (close_i + 1 < tokens_len \
+								&& close_i > 1 \
+								&& !token_type_is_end_op(tokens[close_i + 1]));
+		return (command_build_panic_zero(tokens[close_i]->content));
 	}
-	cmd = command_build(tokens + 1, paren_close_idx - 1);
+	cmd = command_build(tokens + 1, close_i - 1);
 	if (!cmd || cmd->type == CMD_INVALID)
 		return (cmd);
 	else if (command_build_is_empty(cmd))
 		return (command_build_panic_one(cmd, \
-			tokens[paren_close_idx]->content));
+			tokens[close_i]->content));
 	return (command_paren_new(cmd));
 }
 
--- src/internal/repl/shell/token/token_type.c
+++ src/internal/repl/shell/token/token_type.c
@@ -6,7 +6,7 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 20:51:53 by maurodri          #+#    #+#             */
-/*   Updated: 2024/11/13 20:52:21 by maurodri         ###   ########.fr       */
+/*   Updated: 2024/11/14 23:51:29 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,22 @@ int	token_type_is_redirect(t_token *token)
 int	token_type_is_word(t_token *token)
 {
 	return (token && token->type == WORD);
+}
+
+int	token_type_is_either_nl_eof(t_token *token)
+{
+	return (token
+		&& (token->type == OP_NEWLINE
+			|| token->type == OP_EOF));
+}
+
+int	token_type_is_end_op(t_token *token)
+{
+	return (token
+		&& (token->type == OP_EOF
+			|| token->type == OP_NEWLINE
+			|| token->type == OP_PIPE
+			|| token->type == OP_AND
+			|| token->type == OP_OR
+			|| token->type == OP_PAREN_CLOSE));
 }
