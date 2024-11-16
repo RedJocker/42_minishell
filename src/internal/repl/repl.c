2a425b97f4aabd29eb07049e924ceb24cc53725d
dande-je <dande-je@student.42sp.org.br>
2024-11-15 22:44:17 -0300

    Fix: Applying norminette to repl.  Changes to be committed: 	modified:   src/internal/repl/repl.c

--- src/internal/repl/repl.c
+++ src/internal/repl/repl.c
@@ -6,10 +6,13 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 01:15:04 by dande-je          #+#    #+#             */
-/*   Updated: 2024/10/30 03:56:11 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/11/15 21:56:16 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <readline/history.h>
+#include <readline/readline.h>
+#include "ft_memlib.h"
 #include "internal/default.h"
 #include "internal/env/env.h"
 #include "internal/repl/repl.h"
@@ -17,20 +20,8 @@
 #include "internal/repl/terminal/terminal.h"
 #include "internal/repl/history/history.h"
 #include "internal/repl/shell/shell.h"
-#include "ft_memlib.h"
-#include <readline/readline.h>
-#include <readline/history.h>
 
 static void	repl_loop(t_shell *shell);
-
-// void clear_readline_data(void)
-// {
-//     rl_clear_history();
-//     if (rl_line_buffer)
-//         rl_free(rl_line_buffer);
-//     rl_line_buffer = NULL;
-//     rl_done = 1;
-// }
 
 int	repl(void)
 {
@@ -42,8 +33,6 @@ int	repl(void)
 	while (WAIT)
 		repl_loop(&shell);
 	env_destroy();
-	// rl_clear_history();
-	// clear_readline_data();
 	return (signal_status(DEFAULT, GET));
 }
 
